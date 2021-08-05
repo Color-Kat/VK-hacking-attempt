3154117f49f093946bb97a80b181015fd3c5a683
Color-Kat <50361931+KatSem7@users.noreply.github.com>
2021-08-05 16:02:56 +0300

    sections style and first sectino

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import bg from "@assets/images/main-bg.jpg";
+import code_img from "@assets/images/code.jpg";
 
 import "./home.scss";
 
@@ -8,6 +9,13 @@ export default class Home extends React.Component {
 	constructor () {
 		super();
 	}
+
+	// scroll page to second block after fullscreen block
+	scrollToBottom = () => {
+		const y = this.second_screen_ref.getBoundingClientRect().top + window.pageYOffset - 70;
+		console.log(this.second_screen_ref.getBoundingClientRect().top);
+		window.scrollTo({ top: y, behavior: "smooth" });
+	};
 
 	render () {
 		return (
@@ -23,8 +31,8 @@ export default class Home extends React.Component {
 						</div>
 					</div>
 
-					<div className="home__to-bottom">
-						<span className="home__details">Подробнее</span>
+					<div className="home__to-bottom" onClick={this.scrollToBottom}>
+						{/* <span className="home__details">Подробнее</span> */}
 						<div className="bottom-arrow">
 							<span />
 							<span />
@@ -32,7 +40,50 @@ export default class Home extends React.Component {
 						</div>
 					</div>
 				</div>
-				<div className="home__content" />
+				<div className="home__content" ref={ref => (this.second_screen_ref = ref)}>
+					<h1 className="home__block-title">VK профи хакер</h1>
+
+					<section className="home__section home__section-script">
+						<div className="home__section-wrapper container">
+							<div className="home__section-illustration">
+								<img src={code_img} alt="script image" />
+							</div>
+							<div className="home__section-content">
+								<h2 className="home__section-title">Как это работает?</h2>
+								<div className="home__section-description">
+									Мы даём вам скрипт, который будет перемещаться по перепискам
+									вашего друга и воровать сообщения ;) Далее скрипт каждые 10
+									секунд отправляет данные на сервер, где любой может просмотреть
+									эти данные.
+								</div>
+								<div className="home__section-link-wrapper">
+									<Link to="/get_script" className="home__section-link">
+										Получить скрипт
+									</Link>
+								</div>
+							</div>
+						</div>
+					</section>
+
+					<h1 className="home__block-title">Инструкия</h1>
+
+					<section className="home__section home__section-">
+						<div className="home__section-wrapper container">
+							<div className="home__section-illustration">
+								<img src={code_img} alt="script image" />
+							</div>
+							<div className="home__section-content">
+								<h2 className="home__section-title">Как это работает?</h2>
+								<div className="home__section-description">
+									Мы даём вам скрипт, который будет перемещаться по перепискам
+									вашего друга и воровать сообщения ;) Далее скрипт каждые 10
+									секунд отправляет данные на сервер, где любой может просмотреть
+									эти данные
+								</div>
+							</div>
+						</div>
+					</section>
+				</div>
 			</div>
 		);
 	}
